ab25285ab156b69417741e97978a0a67df19dfa9
Wesley Garcia <wes@alltradestech.com>
2022-05-11 13:53:58 -0700

    Add test for Authors#index User Story 1

---
+++ spec/features/authors/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Authors index page" do
+  describe "User Story 1, Parent Index" do
+    # For each parent table
+    # As a visitor
+    # When I visit '/parents'
+    # Then I see the name of each parent record in the system
+    author1 = Author.create!(name: "Stephen King", still_active: true, age: 74)
+    author2 = Author.create!(name: "Jen Gunter", still_active: true, age: 55)
+    visit '/authors'
+
+    expect(page).to have_content(author1.name)
+    expect(page).to have_content(author2.name)
+  end
+end
