699dea3e7df76eb1d882d6d41d83c16c83992287
Wesley Garcia <wes@alltradestech.com>
2022-05-11 14:00:48 -0700

    Update test for Authors#index User Story 1

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -6,11 +6,13 @@ RSpec.describe "Authors index page" do
     # As a visitor
     # When I visit '/parents'
     # Then I see the name of each parent record in the system
-    author1 = Author.create!(name: "Stephen King", still_active: true, age: 74)
-    author2 = Author.create!(name: "Jen Gunter", still_active: true, age: 55)
-    visit '/authors'
+    it "can display the name of each Author" do
+      author1 = Author.create!(name: "Stephen King", still_active: true, age: 74)
+      author2 = Author.create!(name: "Jen Gunter", still_active: true, age: 55)
+      visit '/authors'
 
-    expect(page).to have_content(author1.name)
-    expect(page).to have_content(author2.name)
+      expect(page).to have_content(author1.name)
+      expect(page).to have_content(author2.name)
+    end
   end
 end
