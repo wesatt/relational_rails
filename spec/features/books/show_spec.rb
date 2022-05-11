506dcf2bd204703a89f5c3be34c7f1e5860713a5
Wesley Garcia <wes@alltradestech.com>
2022-05-11 15:50:47 -0700

    Add test for Books#show User story 4

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Books index page" do
-  describe "User Story 3, Child Index" do
+RSpec.describe "Books show page" do
+  describe "User Story 4, Child Show" do
     # As a visitor
-    # When I visit '/child_table_name'
-    # Then I see each Child in the system including the Child's attributes:
-    it "can display the name and attributes of each Book" do
+    # When I visit '/child_table_name/:id'
+    # Then I see the child with that id including the child's attributes:
+    it "can display the name and attributes of a specific Book" do
       book1 = Book.create!(name: "The Gunslinger", has_foreword: true, pages: 100)
       book2 = Book.create!(name: "The Stand", has_foreword: false, pages: 200)
       book3 = Book.create!(name: "The Vagina Bible", has_foreword: true, pages: 100)
       book4 = Book.create!(name: "The Menopause Manifesto", has_foreword: false, pages: 200)
-      visit '/books'
+      visit "/books/#{book1.id}"
 
       # save_and_open_page
       expect(page).to have_content(book1.id)
@@ -20,9 +20,9 @@ RSpec.describe "Books index page" do
       expect(page).to have_content(book1.created_at)
       expect(page).to have_content(book1.updated_at)
       expect(page).to have_content(book1.author_id)
-      expect(page).to have_content(book2.name)
-      expect(page).to have_content(book3.name)
-      expect(page).to have_content(book4.name)
+      expect(page).to_not have_content(book2.name)
+      expect(page).to_not have_content(book3.name)
+      expect(page).to_not have_content(book4.name)
     end
   end
 end
