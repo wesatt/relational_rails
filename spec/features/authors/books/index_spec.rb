be07ba3d75fa701bf4af61f5e4e6297d34c0835f
Wesley Garcia <wes@alltradestech.com>
2022-05-12 14:27:56 -0700

    Add test for Authors/:id/books#index

--- spec/features/authors/books/index_spec.rb
+++ spec/features/authors/books/index_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Books index page" do
-  describe "User Story 3, Child Index" do
+RSpec.describe "Author books index" do
+  describe "User Story 5, Parent Children Index" do
     # As a visitor
-    # When I visit '/child_table_name'
-    # Then I see each Child in the system including the Child's attributes:
-    it "can display the name and attributes of each Book" do
+    # When I visit '/parents/:parent_id/child_table_name'
+    # Then I see each Child that is associated with that Parent with each Child's attributes:
+    it "displays the name and attributes of all books by a specific author" do
       author1 = Author.create!(name: "Stephen King", still_active: true, age: 74)
       author2 = Author.create!(name: "Jen Gunter", still_active: true, age: 55)
       book1 = Book.create!(name: "The Gunslinger", has_foreword: true, pages: 100, author: author1)
       book2 = Book.create!(name: "The Stand", has_foreword: false, pages: 200, author_id: author1.id)
       book3 = author2.books.create!(name: "The Vagina Bible", has_foreword: true, pages: 100)
       book4 = author2.books.create!(name: "The Menopause Manifesto", has_foreword: false, pages: 200)
-      visit '/books'
+
+      visit "author/#{author1.id}/books"
 
       # save_and_open_page
       expect(page).to have_content(book1.id)
@@ -23,8 +24,9 @@ RSpec.describe "Books index page" do
       expect(page).to have_content(book1.updated_at)
       expect(page).to have_content(book1.author_id)
       expect(page).to have_content(book2.name)
-      expect(page).to have_content(book3.name)
-      expect(page).to have_content(book4.name)
+
+      expect(page).to_not have_content(book3.name)
+      expect(page).to_not have_content(book4.name)
     end
   end
 end
