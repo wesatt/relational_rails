0ba1d90c51f1e1f8299f392cbb6a2bef002c509f
Wesley Garcia <wes@alltradestech.com>
2022-05-14 17:28:41 -0700

    Implement code for User story 8 and 9)

--- spec/features/authors/books/index_spec.rb
+++ spec/features/authors/books/index_spec.rb
@@ -30,10 +30,7 @@ RSpec.describe "Author books index" do
     end
   end
 
-  describe "User Story 8, Child Index Link" do
-    # As a visitor
-    # When I visit any page on the site
-    # Then I see a link at the top of the page that takes me to the Child Index
+  describe "User Story 8, Child Index Link & User Story 9, Parent Index Link" do
     let(:author1) { Author.create!(name: "Stephen King", still_active: true, age: 74) }
     let(:author2) { Author.create!(name: "Jen Gunter", still_active: true, age: 55) }
     let(:book1) { Book.create!(name: "The Gunslinger", has_foreword: true, pages: 100, author: author1) }
@@ -42,28 +39,21 @@ RSpec.describe "Author books index" do
     let(:book4) { author2.books.create!(name: "The Menopause Manifesto", has_foreword: false, pages: 200) }
     let(:spec_site_path) { "/authors/#{author1.id}/books" }
 
-    it "has a link to Books#index" do
+    it "has a link to Books#index (User Story 8)" do
+      # As a visitor
+      # When I visit any page on the site
+      # Then I see a link at the top of the page that takes me to the Child Index
       visit spec_site_path
 
       click_link "Books Home"
 
       expect(current_path).to eq("/books")
     end
-  end
 
-  describe "User Story 9, Parent Index Link" do
-    # As a visitor
-    # When I visit any page on the site
-    # Then I see a link at the top of the page that takes me to the Parent Index
-    let(:author1) { Author.create!(name: "Stephen King", still_active: true, age: 74) }
-    let(:author2) { Author.create!(name: "Jen Gunter", still_active: true, age: 55) }
-    let(:book1) { Book.create!(name: "The Gunslinger", has_foreword: true, pages: 100, author: author1) }
-    let(:book2) { Book.create!(name: "The Stand", has_foreword: false, pages: 200, author_id: author1.id) }
-    let(:book3) { author2.books.create!(name: "The Vagina Bible", has_foreword: true, pages: 100) }
-    let(:book4) { author2.books.create!(name: "The Menopause Manifesto", has_foreword: false, pages: 200) }
-    let(:spec_site_path) { "/authors/#{author1}/books" }
-
-    it "has a link to Authors#index" do
+    it "has a link to Authors#index (User story 9)" do
+      # As a visitor
+      # When I visit any page on the site
+      # Then I see a link at the top of the page that takes me to the Parent Index
       visit spec_site_path
 
       click_link "Authors Home"
