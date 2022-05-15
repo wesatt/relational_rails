0f14a982f1d13edb6cb8764d60e391a5032e56fb
Wesley Garcia <wes@alltradestech.com>
2022-05-14 21:07:37 -0700

    Implement functionality for User story 11

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,18 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    new_author = Author.create(param_param_param_pam_pa)
+    new_author.save
+    redirect_to "/authors"
+  end
+
+  private
+  def param_param_param_pam_pa
+    params.permit(:name, :still_active, :age)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/authors', to: 'authors#index'
+  post '/authors', to: 'authors#create'
+  get '/authors/new', to: 'authors#new'
   get '/authors/:id', to: 'authors#show'
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
