0a6852f67638124897dd3c888bd1055ccd97e051
Wesley Garcia <wes@alltradestech.com>
2022-05-14 22:36:28 -0700

    Add tests for User Story 13

--- spec/features/authors/books/index_spec.rb
+++ spec/features/authors/books/index_spec.rb
@@ -61,4 +61,45 @@ RSpec.describe "Author books index" do
       expect(current_path).to eq("/authors")
     end
   end
+
+  describe "User Story 13, Parent Child Creation" do
+    # As a visitor
+    # When I visit a Parent Childs Index page
+    # Then I see a link to add a new adoptable child for that parent "Create Child"
+    # When I click the link
+    # I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
+
+    # When I fill in the form with the child's attributes:
+    # And I click the button "Create Child"
+    # Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
+    # a new child object/row is created for that parent,
+    # and I am redirected to the Parent Childs Index page where I can see the new child listed
+    it "has a link to add a new book to the author" do
+      author1 = Author.create!(name: "Stephen King", still_active: true, age: 74)
+      visit "/authors/#{author1.id}/books"
+
+      expect(page).to have_link("Add Book")
+
+      click_link "Add Book"
+
+      expect(current_path).to eq("/authors/#{author1.id}/books/new")
+    end
+
+    it "creates a new book for the given author" do
+      author1 = Author.create!(name: "Stephen King", still_active: true, age: 74)
+      visit "/authors/#{author1.id}/books"
+
+      expect(page).to_not have_content("The Gunslinger")
+
+      visit "/authors/#{author1.id}/books/new"
+
+      fill_in(:name, with: 'The Gunslinger')
+      fill_in(:has_foreword, with: true)
+      fill_in(:pages, with: 100)
+      click_button('Add Book')
+
+      expect(current_path).to eq("/authors/#{author1.id}/books")
+      expect(page).to have_content("The Gunslinger")
+    end
+  end
 end
