6a3e01acd6558b1bfe7d17edf4a3b94c7cd42bd2
Wesley Garcia <wes@alltradestech.com>
2022-05-15 12:15:30 -0700

    Implement functionality for User Story 15

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ApplicationRecord
 
   validates_presence_of :name, :pages
   validates :has_foreword, inclusion: [ true, false ]
+
+  def self.with_foreword
+    where(has_foreword: true)
+  end
 end
